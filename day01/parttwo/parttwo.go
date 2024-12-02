7a942aa45af85225d15a9e8a24cab2b0a87973da
Lux Barker <29106316+k-nox@users.noreply.github.com>
2024-12-02 15:09:53 -0800

    solve day 1 part 2

---
+++ day01/parttwo/parttwo.go
@@ -0,0 +1,41 @@
+package parttwo
+
+import (
+	"strconv"
+	"strings"
+
+	"github.com/k-nox/advent-of-code-2024/util"
+)
+
+func Solve() int {
+	f := util.NewScannerForInput(1, false)
+	defer f.Close()
+
+	var left []int
+	rightFreq := make(map[int]int)
+
+	for f.Scan() {
+		curr := f.Text()
+		nums := strings.Fields(curr)
+
+		leftNum, err := strconv.Atoi(nums[0])
+		if err != nil {
+			panic(err)
+		}
+
+		rightNum, err := strconv.Atoi(nums[1])
+		if err != nil {
+			panic(err)
+		}
+
+		left = append(left, leftNum)
+		rightFreq[rightNum]++
+	}
+
+	score := 0
+	for _, num := range left {
+		score += (num * rightFreq[num])
+	}
+
+	return score
+}

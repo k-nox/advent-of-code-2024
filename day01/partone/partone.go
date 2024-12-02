cb40a1268b7221b01633af7eec294bf79c56ddc8
Lux Barker <29106316+k-nox@users.noreply.github.com>
2024-12-02 14:56:41 -0800

    solve day 1 part 1

---
+++ day01/partone/partone.go
@@ -0,0 +1,49 @@
+package partone
+
+import (
+	"math"
+	"slices"
+	"strconv"
+	"strings"
+
+	"github.com/k-nox/advent-of-code-2024/util"
+)
+
+func Solve() int {
+	f := util.NewScannerForInput(1, false)
+	defer f.Close()
+	var left []int
+	var right []int
+
+	for f.Scan() {
+		curr := f.Text()
+
+		nums := strings.Fields(curr)
+
+		leftNum, err := strconv.Atoi(nums[0])
+		if err != nil {
+			panic(err)
+		}
+
+		rightNum, err := strconv.Atoi(nums[1])
+		if err != nil {
+			panic(err)
+		}
+
+		left = append(left, leftNum)
+		right = append(right, rightNum)
+	}
+
+	slices.Sort(left)
+	slices.Sort(right)
+
+	distance := 0
+
+	for i, leftNum := range left {
+		rightNum := right[i]
+
+		distance += int(math.Abs(float64(leftNum) - float64(rightNum)))
+	}
+
+	return distance
+}

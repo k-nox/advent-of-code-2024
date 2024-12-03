9d6e450550906964e94f6b010fc2640fb019a1ca
Lux Barker <29106316+k-nox@users.noreply.github.com>
2024-12-03 09:36:59 -0800

    solve day 3 part 2

--- day03/parttwo/parttwo.go
+++ day03/parttwo/parttwo.go
@@ -1,5 +1,51 @@
 package parttwo
 
+import (
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
 func Solve() int {
-	return 0
+	re := regexp.MustCompile(`mul\((\d{1,3},\d{1,3})\)|(do\(\))|(don't\(\))`)
+
+	f, err := os.ReadFile("day03/input/input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	matches := re.FindAllStringSubmatch(string(f), -1)
+
+	total := 0
+	enabled := true
+	for _, match := range matches {
+		label := match[0]
+		if label == "do()" {
+			enabled = true
+			continue
+		}
+
+		if label == "don't()" {
+			enabled = false
+			continue
+		}
+
+		if enabled {
+			nums := strings.Split(match[1], ",")
+			a, err := strconv.Atoi(nums[0])
+			if err != nil {
+				panic(err)
+			}
+
+			b, err := strconv.Atoi(nums[1])
+			if err != nil {
+				panic(err)
+			}
+
+			total += (a * b)
+		}
+	}
+
+	return total
 }

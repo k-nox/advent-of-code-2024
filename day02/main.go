0740cb6790438dbc13cf9a13bdb473755e2e590a
Lux Barker <29106316+k-nox@users.noreply.github.com>
2024-12-02 17:36:42 -0800

    solve day 2 part 2

--- day02/main.go
+++ day02/main.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 
 	"github.com/k-nox/advent-of-code-2024/day02/partone"
+	"github.com/k-nox/advent-of-code-2024/day02/parttwo"
 )
 
 func main() {
 	fmt.Printf("solution for part one: %d\n", partone.Solve())
+	fmt.Printf("solution for part two: %d\n", parttwo.Solve())
 }

7bc2734ba0abeee9a06703f724e6c84b0722c1fe
Lux Barker <29106316+k-nox@users.noreply.github.com>
2024-12-02 17:36:42 -0800

    solve day 2 part 2

--- day02/parttwo/parttwo.go
+++ day02/parttwo/parttwo.go
@@ -1,1 +1,93 @@
 package parttwo
+
+import (
+	"math"
+	"slices"
+	"strconv"
+	"strings"
+
+	"github.com/k-nox/advent-of-code-2024/util"
+)
+
+func Solve() int {
+	f := util.NewScannerForInput(2, false)
+	defer f.Close()
+	safe := 0
+	// threshold := 20
+
+	for f.Scan() {
+		curr := f.Text()
+		rawReport := strings.Fields(curr)
+		report := make([]int, len(rawReport))
+		for i, s := range rawReport {
+			num, err := strconv.Atoi(s)
+			if err != nil {
+				panic(err)
+			}
+			report[i] = num
+		}
+
+		if isSafeReport(report, false) {
+			safe++
+		}
+
+	}
+
+	return safe
+}
+
+func isSafeReport(report []int, toleranceUsed bool) bool {
+	left := 0
+	right := 1
+	isIncreasing := false
+	if report[right] > report[left] {
+		isIncreasing = true
+	}
+
+	isSafe := true
+
+	for right < len(report) {
+		if isSafePair(report[left], report[right], isIncreasing) {
+			left++
+			right++
+		} else {
+			isSafe = false
+			break
+		}
+	}
+
+	if !isSafe && !toleranceUsed {
+		// if right is the last index, we can just remove that to be safe
+		if right == len(report)-1 {
+			isSafe = true
+			// check if removing the first element in the makes the report safe
+		} else if isSafeReport(report[1:], true) {
+			isSafe = true
+			// check if removing the left element makes it safe
+		} else if isSafeReport(slices.Delete(slices.Clone(report), left, left+1), true) {
+			isSafe = true
+			// check if removing the right element makes it safe
+		} else if isSafeReport(slices.Delete(slices.Clone(report), right, right+1), true) {
+			isSafe = true
+		}
+	}
+
+	return isSafe
+}
+
+func isSafePair(prev int, curr int, increasing bool) bool {
+	if increasing && prev >= curr {
+		return false
+	}
+
+	if !increasing && prev <= curr {
+		return false
+	}
+
+	difference := math.Abs(float64(curr) - float64(prev))
+	if difference > 3 || difference < 1 {
+		return false
+	}
+
+	return true
+}

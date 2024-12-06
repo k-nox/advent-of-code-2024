052c0883754d7647b0add01cf8afc506bb8b8b61
Lux Barker <29106316+k-nox@users.noreply.github.com>
2024-12-05 16:39:57 -0800

    use registry type from aoc module

--- cli/registry.go
+++ cli/registry.go
@@ -1,9 +1,11 @@
+
 // Code generated; DO NOT EDIT.
 // This file was generated at
-// 2024-12-05 15:02:05.011051 -0800 PST m=+0.003849001
+// 2024-12-05 16:38:31.983197 -0800 PST m=+0.003955292
 package cli
 
 import (
+	"github.com/k-nox/aoc/cli"
 	"github.com/k-nox/advent-of-code-2024/day01"
 	"github.com/k-nox/advent-of-code-2024/day02"
 	"github.com/k-nox/advent-of-code-2024/day03"
@@ -11,16 +13,10 @@ import (
 	"github.com/k-nox/advent-of-code-2024/day05"
 )
 
-type part func(useSample bool) int
-type day struct {
-	PartOne part
-	PartTwo part
-}
-
-var registry = map[int]day{
-	1: {day01.PartOne, day01.PartTwo},
-	2: {day02.PartOne, day02.PartTwo},
-	3: {day03.PartOne, day03.PartTwo},
-	4: {day04.PartOne, day04.PartTwo},
-	5: {day05.PartOne, day05.PartTwo},
-}
+var registry = cli.Registry{
+	1: {PartOne: day01.PartOne, PartTwo: day01.PartTwo},
+	2: {PartOne: day02.PartOne, PartTwo: day02.PartTwo},
+	3: {PartOne: day03.PartOne, PartTwo: day03.PartTwo},
+	4: {PartOne: day04.PartOne, PartTwo: day04.PartTwo},
+	5: {PartOne: day05.PartOne, PartTwo: day05.PartTwo},
+}	

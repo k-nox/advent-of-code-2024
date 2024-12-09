12906912d69d7b16a16f85fc24a93875fe976ded
Lux Barker <29106316+k-nox@users.noreply.github.com>
2024-12-09 08:20:54 -0800

    solve day 7 part 2

--- day07/parttwo.go
+++ day07/parttwo.go
@@ -1,6 +1,9 @@
 package day07
 
 import (
+	"strconv"
+	"strings"
+
 	"github.com/k-nox/aoc/util"
 )
 
@@ -8,9 +11,43 @@ func PartTwo(useSample bool) int {
 	f := util.NewScannerForInput(7, useSample)
 	defer f.Close()
 
+	sum := 0
 	for f.Scan() {
+		target, vals := parse(f.Text())
+		if isPossibleWithConcat(target, vals) {
+			sum += target
+		}
 
 	}
-	
-	return 0
+
+	return sum
+}
+
+func isPossibleWithConcat(target int, vals []int) bool {
+	if target < 0 {
+		return false
+	}
+	tail := vals[len(vals)-1]
+	if len(vals) == 1 {
+		return target == tail
+	}
+
+	if target%tail == 0 && isPossibleWithConcat(target/tail, vals[:len(vals)-1]) {
+		return true
+	}
+
+	targetStr := strconv.Itoa(target)
+	tailStr := strconv.Itoa(tail)
+	if len(targetStr) > len(tailStr) {
+		newTargetStr, endsWithTail := strings.CutSuffix(strconv.Itoa(target), strconv.Itoa(tail))
+		newTarget, err := strconv.Atoi(newTargetStr)
+		if err != nil {
+			panic(err)
+		}
+		if endsWithTail && isPossibleWithConcat(newTarget, vals[:len(vals)-1]) {
+			return true
+		}
+	}
+
+	return isPossibleWithConcat(target-tail, vals[:len(vals)-1])
 }

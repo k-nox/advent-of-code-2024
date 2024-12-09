1ec0987ba97d7c6ac99764fbf61db287a889b1da
Lux Barker <29106316+k-nox@users.noreply.github.com>
2024-12-09 10:12:02 -0800

    solve day 8 part 2

--- day08/parttwo.go
+++ day08/parttwo.go
@@ -8,9 +8,47 @@ func PartTwo(useSample bool) int {
 	f := util.NewScannerForInput(8, useSample)
 	defer f.Close()
 
-	for f.Scan() {
+	grid, antennas := parse(f)
+	uniqueAntinodes := map[util.Point]bool{}
 
+	for _, locations := range antennas {
+		for i := 0; i < len(locations)-1; i++ {
+			for j := 0; j < len(locations); j++ {
+				if i == j {
+					continue
+				}
+				antinodesForPair := findAllAntinodesForPair(grid, locations[i], locations[j])
+				for _, n := range antinodesForPair {
+					if _, ok := uniqueAntinodes[n]; !ok {
+						uniqueAntinodes[n] = true
+					}
+				}
+			}
+		}
 	}
-	
-	return 0
+
+	return len(uniqueAntinodes)
+}
+
+func findAllAntinodesForPair(g util.Grid, a util.Point, b util.Point) []util.Point {
+	antinodes := []util.Point{a, b}
+	distance := a.Sub(b)
+	antinodeA := a.Add(distance)
+	_, aOk := g[antinodeA]
+	for aOk {
+		// keep adding distance to a until it's not valid
+		antinodes = append(antinodes, antinodeA)
+		antinodeA = antinodeA.Add(distance)
+		_, aOk = g[antinodeA]
+	}
+
+	antinodeB := b.Sub(distance)
+	_, bOk := g[antinodeB]
+	for bOk {
+		antinodes = append(antinodes, antinodeB)
+		antinodeB = antinodeB.Sub(distance)
+		_, bOk = g[antinodeB]
+	}
+
+	return antinodes
 }

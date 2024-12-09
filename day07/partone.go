05972ad68b25f77429cf2087cd25bb464c8101d7
Lux Barker <29106316+k-nox@users.noreply.github.com>
2024-12-09 07:45:26 -0800

    solve day 7 part 1

--- day07/partone.go
+++ day07/partone.go
@@ -1,6 +1,9 @@
 package day07
 
 import (
+	"strconv"
+	"strings"
+
 	"github.com/k-nox/aoc/util"
 )
 
@@ -8,9 +11,50 @@ func PartOne(useSample bool) int {
 	f := util.NewScannerForInput(7, useSample)
 	defer f.Close()
 
+	sum := 0
 	for f.Scan() {
-
+		target, vals := parse(f.Text())
+		if isPossible(target, vals) {
+			sum += target
+		}
 	}
 
-	return 0
+	return sum
+}
+
+func parse(line string) (int, []int) {
+	var (
+		target int
+		vals   []int
+		err    error
+	)
+	parts := strings.Split(line, ":")
+
+	target, err = strconv.Atoi(parts[0])
+	if err != nil {
+		panic(err)
+	}
+
+	rawVals := strings.Fields(parts[1])
+	for _, rawVal := range rawVals {
+		val, err := strconv.Atoi(rawVal)
+		if err != nil {
+			panic(err)
+		}
+		vals = append(vals, val)
+	}
+
+	return target, vals
+}
+
+func isPossible(target int, vals []int) bool {
+	tail := vals[len(vals)-1]
+	if len(vals) == 1 {
+		return target == tail
+	}
+
+	if target%tail == 0 && isPossible(target/tail, vals[:len(vals)-1]) {
+		return true
+	}
+	return isPossible(target-tail, vals[:len(vals)-1])
 }

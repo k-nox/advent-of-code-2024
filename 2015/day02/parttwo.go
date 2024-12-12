ed84ff2d3609f40e93376a4ac0f7004b91b813ab
Lux Barker <29106316+k-nox@users.noreply.github.com>
2024-12-12 14:11:38 -0800

    solve year 2015 day 2 part 2

--- 2015/day02/parttwo.go
+++ 2015/day02/parttwo.go
@@ -2,6 +2,9 @@ package day02
 
 import (
 	"bufio"
+	"regexp"
+	"slices"
+	"strconv"
 
 	"github.com/k-nox/advent-of-code-solutions/helper"
 )
@@ -12,9 +15,29 @@ func PartTwo(useSample bool) int {
 
 	scanner := bufio.NewScanner(f)
 
+	re := regexp.MustCompile(`\d+`)
+	total := 0
 	for scanner.Scan() {
-	
+		dimensions := re.FindAllString(scanner.Text(), -1)
+		l, err := strconv.Atoi(dimensions[0])
+		if err != nil {
+			panic(err)
+		}
+		w, err := strconv.Atoi(dimensions[1])
+		if err != nil {
+			panic(err)
+		}
+		h, err := strconv.Atoi(dimensions[2])
+		if err != nil {
+			panic(err)
+		}
+		total += slices.Min(perimeters(l, w, h))
+		total += (l * w * h)
 	}
 
-	return 0
+	return total
+}
+
+func perimeters(l int, w int, h int) []int {
+	return []int{2 * (l + w), 2 * (w + h), 2 * (h + l)}
 }
